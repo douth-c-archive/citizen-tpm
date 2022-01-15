1f4f6bfc672e8bda4f99e532aedd4593ca4a6c98
BombayV <OMGmsebastian@gmail.com>
2022-01-15 03:46:54 -0500

    feat(client): interval handling

--- client.lua
+++ client.lua
@@ -8,48 +8,69 @@ TriggerEvent('chat:addSuggestion', '/' .. commandName, "Teleport to your marked 
 local function teleportToWaypoint()
     local blipMarker <const> = GetFirstBlipInfoId(8)
     if DoesBlipExist(blipMarker) then
-        local coords, found, zState = GetBlipInfoIdCoord(blipMarker), true, -1.0
-        local x, y, z = coords['x'], coords['y'], 1500.0
-        SetTimeout(5000, function() if found then found = false end end)
-        while found do
-            local groundFound, groundZ = GetGroundZFor_3dCoord(x + 0.0, y + 0.0, z + zState)
-            if z < 0 then
-                zState = 1.0
-            elseif z > 1500 then
-                zState = -1.0
+        DoScreenFadeOut(500)
+        while not IsScreenFadedOut() do
+            Wait(0)
+        end
+        local ped, coords <const> = PlayerPedId(), GetBlipInfoIdCoord(blipMarker)
+        local oldCoords = GetEntityCoords(ped)
+        local x, y, groundZ, Z_START = coords['x'], coords['y'], 850.0, 950.0
+        local found = false
+        FreezeEntityPosition(ped, true)
+        for i = Z_START, 0, -25.0 do
+            local z = i
+            -- vmenu implementation
+            if (i % 2) ~= 0 then
+                z = Z_START - i
             end
-            if groundFound then
-                z = groundZ + 0.0
-                SetPedCoordsKeepVehicle(PlayerPedId(), x, y, z)
-                return true
+
+            RequestCollisionAtCoord(x, y, z);
+            NewLoadSceneStart(x, y, z, x, y, z, 50.0, 0)
+
+            local curTime = GetGameTimer()
+            while IsNetworkLoadingScene() do
+                if GetGameTimer() - curTime > 1000 then
+                    break
+                end
+                Wait(0)
+            end
+            SetEntityCoords(ped, x, y, z, false, false, false, true)
+
+            while not HasCollisionLoadedAroundEntity(ped) do
+                RequestCollisionAtCoord(x, y, z);
+                if GetGameTimer() - curTime > 1000 then
+                    break
+                end
+                Wait(0)
+            end
+            NewLoadSceneStop()
+            found, groundZ = GetGroundZFor_3dCoord(x, y, z, false);
+            if found then
+                Wait(0)
+                SetEntityCoords(ped, x, y, groundZ, false, false, false, true)
+                break
             end
             Wait(0)
         end
+
+        DoScreenFadeIn(500);
+        if not found then
+            print('not found')
+            SetEntityCoords(ped, oldCoords['x'], oldCoords['y'], oldCoords['z'] - 1.0, false, false, false, true)
+            FreezeEntityPosition(ped, false)
+            return false
+        end
+        FreezeEntityPosition(ped, false)
+        RequestCollisionAtCoord(x, y, groundZ);
+        SetEntityCoords(ped, x, y, groundZ, false, false, false, true)
+        --print(x, y, groundZ)
+        return true
     end
     return false
 end
 
 RegisterCommand(commandName, function()
     local hasTeleported <const> = teleportToWaypoint()
-    if not hasTeleported then
-        if chatRem then
-            TriggerEvent('chat:addMessage', {
-                color = {255,85,85},
-                args = {"citizen-tpm", "could not find ground at this waypoint."}
-            })
-        end
-        if consoleRem then
-            print("could not find ground")
-        end
-        return
-    end
-    if chatRem then
-        TriggerEvent('chat:addMessage', {
-            color = {255,85,85},
-            args = {"citizen-tpm", "client needs to set a waypoint first."}
-        })
-    end
-    if consoleRem then
-        print("client needs to set a waypoint first")
-    end
 end)
+
+exports('TeleportToWaypoint', teleportToWaypoint)
